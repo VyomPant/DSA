10ede19db97891b8d5cbf00962e8791b5d7a00dc
VyomPant <vyompant14@gmail.com>
2025-10-01 21:02:09 +0530

    Vyom | refactor Car Fleet

--- src/neetCode150/stack/CarFleet.java
+++ src/neetCode150/stack/CarFleet.java
@@ -1,6 +1,6 @@
 package neetCode150.stack;
 
-/* https://leetcode.com/problems/car-fleet/description/
+/** https://leetcode.com/problems/car-fleet/description/
 There are n cars at given miles away from the starting mile 0, traveling to reach the mile target.
 
 You are given two integer arrays position and speed, both of length n, where position[i] is the starting mile of the ith car and speed[i] is the speed of the ith car in miles per hour.
@@ -32,7 +32,7 @@ import java.util.Stack;
 
 class CarFleet {
 
-    /*
+    /**
      * Optimized Solution using a Stack
      * Time Complexity: O(nlogn)
      * Space Complexity: O(n)
